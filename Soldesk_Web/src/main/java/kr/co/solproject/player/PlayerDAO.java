e6fdf504c1937bcad5a0e59e68b1d56246b1205b
mirae8098 <kimmirae8098@gmail.com>
2015-12-11 09:50:34 +0900

    동영상 메모 + 목록 2

--- Soldesk_Web/src/main/java/kr/co/solproject/player/PlayerDAO.java
+++ Soldesk_Web/src/main/java/kr/co/solproject/player/PlayerDAO.java
@@ -1,5 +1,6 @@
 package kr.co.solproject.player;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -16,26 +17,47 @@ public class PlayerDAO {
     System.out.println("PlayerDAO 객체 생성");
   }
   
+  // admin page에서 categoryno 가져오기 
   public int getCategoryno(Map map){
     int categoryno = 0;
     try{
-      categoryno = (Integer) mybatis.queryForObject("sol_category.read", map);
+      categoryno = (Integer) mybatis.queryForObject("sol_category.categorynoRead", map);
     }catch (Exception e){
       System.out.println("실패: "+e);
     }
     return categoryno;
   }
   
+  // admin page 에서 동영상 등록 
   public boolean playerInsert(PlayerDTO dto){
-    
     boolean flag = false;
     try{
-      int cnt = (Integer) mybatis.insert("sol_category.insert", dto);
+      int cnt = mybatis.update("sol_category.lectureInsert", dto);
       if(cnt>0) flag = true;
     }catch (Exception e){
       System.out.println("실패다: "+e);
     }
     return flag;
+  }
+  
+  public PlayerDTO read(int lectureno){
+    PlayerDTO dto = null;
+    try{
+      dto = (PlayerDTO) mybatis.queryForObject("sol_category.lectureRead", lectureno);
+    }catch (Exception e){
+      System.out.println("read 실패다: "+e);
+    }
+    return dto;
+  }
+  
+  public List list(int categoryno){
+    List list = null;
+    try{
+      list = mybatis.queryForList("sol_category.lectureList", categoryno);
+    }catch (Exception e){
+      System.out.println("read 실패다: "+e);
+    }
+    return list;
   }
   
 }
