1492424dcc00b93d59803bfe42df25ededc25e2f
민경 <alsrud0482@gmail.com>
2015-12-23 19:38:42 +0900

    문제풀기 myscore 점수칼럼 추가/q&a버튼수정

--- Soldesk_Web/src/main/webapp/sol_sql/exam.sql
+++ Soldesk_Web/src/main/webapp/sol_sql/exam.sql
@@ -26,7 +26,8 @@ create table sol_myscore(
 	testno		INT				NOT NULL,
 	usetime		VARCHAR(20)		NOT NULL,
 	answerno	VARCHAR(200)	NOT NULL,				
-	regdt		datetime		NOT NULL,	
+	regdt		datetime		NOT NULL,
+	myscore		INT				NOT NULL,
 	primary key(mytestno)						
 )ENGINE=MyISAM DEFAULT CHARSET="euckr";							
 
